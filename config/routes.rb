b6f5e41ce18f63167c6deaa1a2f440621fdb1cff
Marks <artmar89@gmail.com>
2013-08-16 12:04:51 -0500

    bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 RubyRails4Sample::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  get 'home/index'
 
+  root 'home#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
